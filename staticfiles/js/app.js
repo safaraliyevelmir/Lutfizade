007da47af2faef8820a27244578f1aee26f283e7
safaraliyevelmir <safaraliyevelmir@gmail.com>
2021-07-26 15:01:39 +0100

    static

---
+++ staticfiles/js/app.js
@@ -0,0 +1,35 @@
+var mybutton = document.getElementById("upBtn");
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+      mybutton.style.display = "block";
+    } else {
+      mybutton.style.display = "none";
+    }
+  }
+
+  function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+  }
+
+
+
+function dropBtn() {
+  document.getElementById("dropdown").classList.toggle("open");
+}
+
+window.onclick = function(event) {
+  if (!event.target.matches('.menu-btn')) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('open')) {
+        openDropdown.classList.remove('open');
+      }
+    }
+  }
+}
